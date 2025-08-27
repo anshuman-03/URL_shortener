69b7e922bf1f9d6ae6f99762713d0de79e94dc3f
anshuman-03 <anshuman0120@gmail.com>
2025-08-27 08:56:10 +0530

    port updated

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ const server = createServer(async (req, res) => {
     }
 });
 
-server.listen(PORT, () => {
+server.listen(PORT,"0.0.0.0", () => {
     console.log(`Server runnning at http://localhost:${PORT}`);
 })
